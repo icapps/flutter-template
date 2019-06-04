720af74f9cb3fb90561c6b7c1c4bd45347db3416
Koen Van Looveren <koen.vanlooveren@icapps.com>
2019-06-04 15:45:26 +0200

    Added network call method to the logger

--- lib/util/logger/logger.dart
+++ lib/util/logger/logger.dart
@@ -6,12 +6,12 @@ class Logger {
 
   static void logNetworkRequest(RequestOptions options) {
     if (!FlavorConfig.instance.values.logNetworkInfo) return;
-    print('---------------> url: ${options.uri.toString()}');
+    print('---------------> ${options.method} - url: ${options.uri.toString()}');
   }
 
   static void logNetworkResponse(Response response) {
     if (!FlavorConfig.instance.values.logNetworkInfo) return;
-    print('<--------------- url: ${response.request.uri.toString()} - statucode: ${response.statusCode}');
+    print('<--------------- ${response.request.method} - url: ${response.request.uri.toString()} - statucode: ${response.statusCode}');
   }
 
   static logNetworkError(DioError error) {
@@ -23,6 +23,6 @@ class Logger {
       print('response.data | ${error.response.data}');
       print('response.headers | ${error.response.headers}');
     }
-    print('<--------------- url: ${error.request.uri.toString()} - statucode: ${error.response.statusCode}');
+    print('<--------------- ${error.request.method} - url: ${error.request.uri.toString()} - statucode: ${error.response.statusCode}');
   }
 }
