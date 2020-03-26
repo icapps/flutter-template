e8bb47d1da122ba74393e80b3d67c8de3f2ad96f
Koen Van Looveren <koen.vanlooveren@icapps.com>
2020-03-26 15:37:36 +0100

    Added tests for the todo model

---
+++ test/model/webservice/todo/todo_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_template/model/webservice/todo/todo.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../di/test_kiwi_util.dart';
+
+void main() {
+  setUp(() async => TestKiwiUtil.init());
+
+  test('TodoTest', () async {
+    final todo = Todo(id: 1, title: 'title', completed: true);
+
+    //PARSE TO JSON
+    final json = todo.toJson();
+    expect(json['id'], todo.id);
+    expect(json['title'], todo.title);
+    expect(json['completed'], todo.completed);
+
+    //PARSE FROM JSON
+    final parsedTodo = Todo.fromJson(json);
+    expect(parsedTodo.id, todo.id);
+    expect(parsedTodo.title, todo.title);
+    expect(parsedTodo.completed, todo.completed);
+  });
+}
