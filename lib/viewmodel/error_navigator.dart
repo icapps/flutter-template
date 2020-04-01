07ed3edba853002e43a1e6a104fac294eb734e48
Koen Van Looveren <koen.vanlooveren@icapps.com>
2020-04-01 10:07:22 +0200

    Added error navigator and back navigator tests

--- lib/viewmodel/error_navigator.dart
+++ lib/viewmodel/error_navigator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/styles/theme_text_styles.dart';
 import 'package:flutter_template/util/locale/localization.dart';
 
 abstract class ErrorNavigator {
@@ -15,7 +16,12 @@ mixin ErrorNavigatorMixin<T extends StatefulWidget> on State<T> implements Error
   void showErrorWithLocaleKey(String errorKey) => _showSnackBar(Localization.of(context).getTranslation(errorKey));
 
   void _showSnackBar(String message) {
-    final snackBar = SnackBar(content: Text(message));
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: ThemeTextStyles.lightButtonTextStyle,
+      ),
+    );
     getScaffoldState()?.showSnackBar(snackBar);
   }
 
---
+++ test/viewmodel/back_navigator_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_template/screen/splash/splash_screen.dart';
+import 'package:flutter_template/viewmodel/back_navigator.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../di/test_kiwi_util.dart';
+import '../mocks/main_navigator/mock_main_navigator_widget.dart';
+import '../screen/seed.dart';
+import '../util/test_extensions.dart';
+import '../util/test_util.dart';
+
+void main() {
+  setUp(() async {
+    await TestKiwiUtil.init();
+  });
+
+  testWidgets('Test splash screen navigator', (tester) async {
+    seedGlobalViewModel();
+
+    final mockNavigation = MockMainNavigation();
+    final debugKey = GlobalKey<BackScreenState>();
+    final sut = MockMainNavigator(
+      mock: mockNavigation,
+      child: BackScreen(key: debugKey),
+    );
+    await TestUtil.loadScreen(tester, sut);
+
+    debugKey.currentState.goBack();
+    verify(mockNavigation.goBack()).calledOnce();
+    verifyNoMoreInteractions(mockNavigation);
+
+    debugKey.currentState.goBack(result: 'testing');
+    verify(mockNavigation.goBack(result: 'testing')).calledOnce();
+    verifyNoMoreInteractions(mockNavigation);
+  });
+}
+
+class BackScreen extends StatefulWidget {
+  const BackScreen({Key key}) : super(key: key);
+
+  @override
+  BackScreenState createState() => BackScreenState();
+}
+
+@visibleForTesting
+class BackScreenState extends State<BackScreen> with BackNavigatorMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
