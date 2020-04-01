93b72adaaf5d253ad62151d108a10dbc44d76328
Koen Van Looveren <koen.vanlooveren@icapps.com>
2020-04-01 10:09:52 +0200

    Fixed the analyzer

--- test/viewmodel/back_navigator_test.dart
+++ test/viewmodel/back_navigator_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_template/screen/splash/splash_screen.dart';
 import 'package:flutter_template/viewmodel/back_navigator.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
