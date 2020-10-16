e4a4e90afd19ba769ef03915444adf8731aa2db8
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-16 20:06:10 +0200

    Renamed the lib & test files and pubspec.yaml

--- tool/dart_tool/rename_project.dart
+++ tool/dart_tool/rename_project.dart
@@ -1,29 +1,63 @@
 import 'dart:io';
 
+const originalProjectName = 'test';
+
 void main() {
   Logger.info('Enter Dart Package Name:');
   final packageName = stdin.readLineSync();
   _renamePackage(packageName);
+  _packagesGet();
 }
 
 void _renamePackage(String packageName) {
   Logger.info('Using `$packageName` as your new dart package name');
-  Logger.info('Renaming Pubspec.yaml ...');
-  _replaceInFile('pubspec.yaml', 'name: flutter_template', 'name: $packageName');
+  Logger.info('Replace text in Pubspec.yaml ...');
+  _replaceInFile('pubspec.yaml', 'name: $originalProjectName', 'name: $packageName');
 
-  Logger.info('Renaming files ...');
+  Logger.info('Replace text in files ...');
   final dir = Directory('lib');
   dir
       .listSync(recursive: true)
       .where((element) => !Directory(element.path).existsSync())
-      .forEach((element) => _replaceInFile(element.path, "import 'package:flutter_template/", "import 'package:$packageName/"));
+      .forEach((element) => _replaceInFile(element.path, "import 'package:$originalProjectName/", "import 'package:$packageName/"));
+
+  Logger.info('Replace text in test files ...');
+  final testDir = Directory('test');
+  testDir
+      .listSync(recursive: true)
+      .where((element) => !Directory(element.path).existsSync())
+      .where((element) => !element.path.endsWith('.png'))
+      .forEach((element) => _replaceInFile(element.path, "import 'package:$originalProjectName/", "import 'package:$packageName/"));
 }
 
+void _packagesGet() {
+  _executeCommand('flutter', ['packages', 'get']);
+}
+
+/// ==============
+/// UTIL FUNCTIONS
+/// ==============
 void _replaceInFile(String path, String originalString, String newString) {
   final file = File(path);
   final original = file.readAsStringSync();
   final newContent = original.replaceAll(originalString, newString);
   file.writeAsStringSync(newContent);
+}
+
+void _executeCommand(String cmd, List<String> params) {
+  final fullCommand = '$cmd ${params.join(' ')}';
+  try {
+    Logger.debug('Executing command:');
+    Logger.info('$fullCommand');
+    final result = Process.runSync(cmd, params);
+    final output = result.stdout;
+    if (output.toString().isNotEmpty) {
+      Logger.debug('$output');
+    }
+  } catch (e) {
+    Logger.error('\nFailed to execute command: $fullCommand\n$e');
+    exit(-1);
+  }
 }
 
 class Logger {
@@ -32,4 +66,5 @@ class Logger {
   static void debug(value) => print(value); // ignore: avoid_print
 
   static void info(value) => print(value); // ignore: avoid_print
+  static void error(value) => print(value); // ignore: avoid_print
 }
