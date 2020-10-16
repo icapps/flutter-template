2421090a1ebe8a6bd5abde37d1b76ee13527b057
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-16 19:56:16 +0200

    Added an extra method to separate the replace logic

--- tool/dart_tool/rename_project.dart
+++ tool/dart_tool/rename_project.dart
@@ -3,18 +3,27 @@ import 'dart:io';
 void main() {
   Logger.info('Enter Dart Package Name:');
   final packageName = stdin.readLineSync();
-  renamePackage(packageName);
+  _renamePackage(packageName);
 }
 
-void renamePackage(String packageName) {
+void _renamePackage(String packageName) {
   Logger.info('Using `$packageName` as your new dart package name');
+  Logger.info('Renaming Pubspec.yaml ...');
+  _replaceInFile('pubspec.yaml', 'name: flutter_template', 'name: $packageName');
+
+  Logger.info('Renaming files ...');
   final dir = Directory('lib');
-  dir.listSync(recursive: true).where((element) => !Directory(element.path).existsSync()).forEach((element) {
-    final file = File(element.path);
-    final original = file.readAsStringSync();
-    final newContent = original.replaceAll("import 'package:flutter_template/", "import 'package:$packageName/");
-    file.writeAsStringSync(newContent);
-  });
+  dir
+      .listSync(recursive: true)
+      .where((element) => !Directory(element.path).existsSync())
+      .forEach((element) => _replaceInFile(element.path, "import 'package:flutter_template/", "import 'package:$packageName/"));
+}
+
+void _replaceInFile(String path, String originalString, String newString) {
+  final file = File(path);
+  final original = file.readAsStringSync();
+  final newContent = original.replaceAll(originalString, newString);
+  file.writeAsStringSync(newContent);
 }
 
 class Logger {
