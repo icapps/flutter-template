2d29231d4a707b4260bdaf385241f1d8231601cf
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-17 09:26:26 +0200

    Fixed the test.sh script to use flutter_template instead of model_generator

--- tool/travis/test.sh
+++ tool/travis/test.sh
@@ -5,7 +5,7 @@ echo "First create a file with all other files imported so flutter test coverage
 file=test/coverage_helper_test.dart
 echo "// Helper file to make coverage work for all dart files\n" > $file
 echo "// ignore_for_file: unused_import" >> $file
-find lib -name '*.dart' | cut -c4- | awk -v package=$1 '{printf "import '\''package:%s%s'\'';\n", "model_generator", $1}' >> $file
+find lib -name '*.dart' | cut -c4- | awk -v package=$1 '{printf "import '\''package:%s%s'\'';\n", "flutter_template", $1}' >> $file
 echo "" >> $file
 echo "void main(){}" >> $file
 echo "==="
