04c95d1426dcdfd36288c8a8800456f59b555634
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-17 09:51:26 +0200

    Fixed the script that creates the coveragehelper

--- tool/travis/test.sh
+++ tool/travis/test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "==="
-echo "First create a file with all other files imported so flutter test coverage uses all files"
-file=test/coverage_helper_test.dart
-echo "// Helper file to make coverage work for all dart files\n" > $file
-echo "// ignore_for_file: unused_import" >> $file
-find lib -name '*.dart' | cut -c4- | awk -v package=$1 '{printf "import '\''package:%s%s'\'';\n", "flutter_template", $1}' >> $file
-echo "" >> $file
-echo "void main(){}" >> $file
-echo "==="
+dart run ./tool/travis/test_full_coverage_test_file_creator.dart || exit -1;
 
 flutter test --coverage || exit -1;
 
