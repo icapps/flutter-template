02a2714af8a022632d2c96a9e1f0f2df04502617
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-17 12:08:54 +0200

    Updated the travis readme.md job

--- tool/travis/rename_project.sh
+++ tool/travis/rename_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf 'Test Project\ndescription\ntest_project\nTestProject\ncom.test.project\nn\n' | dart run ./tool/dart_tool/rename_project.dart || exit -1;
+printf 'Test Project\ndescription\ntest_project\nTestProject\ncom.test.project\nn\ny\n' | dart run ./tool/dart_tool/rename_project.dart || exit -1;
