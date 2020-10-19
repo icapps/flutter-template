d9a89243427b92101cefb6e45aec1f9361d02939
Frederik Leys <frederik.leys@icapps.com>
2020-10-19 13:18:40 +0200

    #71 fvm support added

---
+++ tool/dart_tool/install_fvm.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+main() {
+  final file = File('.fvm/fvm_config.json');
+  final jsonObject = json.decode(file.readAsStringSync());
+  final version = jsonObject['flutterSdkVersion'];
+
+  executeCommand('rm', ['.fvm/flutter_sdk']);
+  executeCommand('fvm', ['version']);
+  executeCommand('fvm', ['install', version]);
+  executeCommand('fvm', ['use', version]);
+}
+
+void executeCommand(String cmd, List<String> params) {
+  final fullCommand = '$cmd ${params.join(' ')}';
+  try {
+    printMessage('\nExecuting command:\n$fullCommand');
+    final result = Process.runSync(cmd, params);
+    final output = result.stdout;
+    if (output.toString().isNotEmpty) {
+      printMessage('$output');
+    }
+  } catch (e) {
+    printMessage('\nFailed to execute command: $fullCommand\n$e');
+  }
+}
+
+void printMessage(Object message) {
+  // ignore: avoid_print
+  print(message);
+}
