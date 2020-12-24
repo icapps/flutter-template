4db2a16c0b7e0426547c9c8ead90934a91b1f697
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-12-24 15:06:17 +0100

    Test

--- tool/dart_tool/strip_boilerplate_project.dart
+++ tool/dart_tool/strip_boilerplate_project.dart
@@ -90,10 +90,8 @@ final removeCodeLines = [
   '  @Register.factory(TodoAddViewModel)',
   '      case TodoAddScreen.routeName:',
   '        return MaterialPageRoute<void>(builder: (context) => const FlavorBanner(child: TodoAddScreen()), settings: settings);',
-  '''
-  @override
-  void goToAddTodo() => navigationKey.currentState.pushNamed(TodoAddScreen.routeName);
-  ''',
+  '''  @override
+  void goToAddTodo() => navigationKey.currentState.pushNamed(TodoAddScreen.routeName);''',
   //Test Lines
   "import '../mocks/database/todo/mock_todo_dao_storage.dart';",
   "import '../mocks/repository/todo/mock_todo_repository.dart';",
