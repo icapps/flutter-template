744c5e5de7423ffe602dcf769a04a6809365026a
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-12-24 14:48:04 +0100

    Moved the transparant_image to the test folder where it is used

--- test/mocks/mocked_network_images.dart
+++ test/mocks/mocked_network_images.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
+import 'dart:typed_data';
 
-import 'package:flutter_template/vendor/transparant_image.dart';
 import 'package:mockito/mockito.dart';
 
 R provideMockedNetworkImages<R>(R body(), {List<int> imageBytes = TransparantImageUtil.transparentImage}) {
@@ -41,3 +41,77 @@ MockHttpClient _createMockImageHttpClient(SecurityContext _, List<int> imageByte
 
   return client;
 }
+
+class TransparantImageUtil {
+  TransparantImageUtil._();
+
+  static final transparentUint8ListImage = Uint8List.fromList(transparentImage);
+
+  static const transparentImage = <int>[
+    0x89,
+    0x50,
+    0x4E,
+    0x47,
+    0x0D,
+    0x0A,
+    0x1A,
+    0x0A,
+    0x00,
+    0x00,
+    0x00,
+    0x0D,
+    0x49,
+    0x48,
+    0x44,
+    0x52,
+    0x00,
+    0x00,
+    0x00,
+    0x01,
+    0x00,
+    0x00,
+    0x00,
+    0x01,
+    0x08,
+    0x06,
+    0x00,
+    0x00,
+    0x00,
+    0x1F,
+    0x15,
+    0xC4,
+    0x89,
+    0x00,
+    0x00,
+    0x00,
+    0x0A,
+    0x49,
+    0x44,
+    0x41,
+    0x54,
+    0x78,
+    0x9C,
+    0x63,
+    0x00,
+    0x01,
+    0x00,
+    0x00,
+    0x05,
+    0x00,
+    0x01,
+    0x0D,
+    0x0A,
+    0x2D,
+    0xB4,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x49,
+    0x45,
+    0x4E,
+    0x44,
+    0xAE,
+  ];
+}
+
