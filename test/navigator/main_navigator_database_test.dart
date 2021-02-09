a3cbbd364b771553b1a0da0aa424fa15c3eb1028
ikbendewilliam <ikbendewilliam@hotmail.com>
2021-02-09 15:23:10 +0100

    Changed test for DB viewer so that it is independent on database entries

--- test/navigator/main_navigator_database_test.dart
+++ test/navigator/main_navigator_database_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_template/database/flutter_template_database.dart';
 import 'package:flutter_template/navigator/main_navigator.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:moor_db_viewer/moor_db_viewer.dart';
 
 import '../di/test_kiwi_util.dart';
 import '../screen/seed.dart';
@@ -13,19 +14,13 @@ void main() {
   });
 
   testWidgets('Test main navigator widget database', (tester) async {
-    seedsLicenses();
     seedGlobalViewModel();
-
     final key = GlobalKey<MainNavigatorWidgetState>();
     final sut = MainNavigatorWidget(key: key);
     final db = TestKiwiUtil.resolve<FlutterTemplateDatabase>();
-    final testWidget = await TestUtil.loadScreen(tester, sut);
-    await TestUtil.takeScreenshotForAllSizes(tester, testWidget, 'main_navigator_database_screen_0_initial_screen');
+    await TestUtil.loadScreen(tester, sut);
     key.currentState.goToDatabase(db);
     await tester.pumpAndSettle();
-    await TestUtil.takeScreenshotForAllSizes(tester, testWidget, 'main_navigator_database_screen_1');
-    key.currentState.goBack<void>();
-    await tester.pumpAndSettle();
-    await TestUtil.takeScreenshotForAllSizes(tester, testWidget, 'main_navigator_database_screen_2_go_back');
+    expect(find.byType(MoorDbViewer), findsOneWidget);
   });
 }
