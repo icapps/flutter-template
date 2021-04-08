dbe49e9d149dca1fb5ed64c2bd278af7978fe9f7
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-04-08 14:43:18 +0200

    Updated the os config to os-info

---
+++ lib/architecture.dart
@@ -0,0 +1,5 @@
+import 'package:icapps_architecture/icapps_architecture.dart';
+
+Future<void> initArchitecture() async {
+  await OsInfo.init();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/architecture.dart';
 import 'package:flutter_template/util/database/database_inspector.dart';
 import 'package:flutter_template/di/environments.dart';
 import 'package:flutter_template/di/injectable.dart';
-import 'package:flutter_template/util/env/os_config.dart';
 import 'package:flutter_template/app.dart';
-import 'package:flutter_template/niddler.dart';
+import 'package:flutter_template/util/niddler/niddler.dart';
 import 'package:flutter_template/util/env/flavor_config.dart';
 
 Future<void> main() async {
@@ -12,7 +12,7 @@ Future<void> main() async {
 
   await initNiddler();
 
-  await OsConfig.init();
+  await initArchitecture();
   const values = FlavorValues(
     baseUrl: 'https://jsonplaceholder.typicode.com/',
     logNetworkInfo: true,
