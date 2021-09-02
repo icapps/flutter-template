74d005006ac82a58dcbf45d537d73caa93e344ce
Nicola Verbeeck <nicola@chimerapps.com>
2021-09-02 13:47:31 +0200

    Add run zone guarded. Fixed #186

---
+++ lib/main_common.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_template/architecture.dart';
+import 'package:icapps_architecture/icapps_architecture.dart';
+import 'package:pedantic/pedantic.dart';
+
+Future<void> _setupCrashLogging({required bool enabled}) async {
+  await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(enabled);
+  unawaited(FirebaseCrashlytics.instance.sendUnsentReports());
+
+  final originalOnError = FlutterError.onError;
+  FlutterError.onError = (errorDetails) async {
+    if (enabled) {
+      await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    }
+    originalOnError?.call(errorDetails);
+  };
+}
+
+FutureOr<R>? wrapMain<R>(FutureOr<R> Function() appCode, {required bool enableCrashLogging}) {
+  return runZonedGuarded(() async {
+    await _setupCrashLogging(enabled: false);
+    await initArchitecture();
+
+    return await appCode();
+  }, (object, trace) {
+    WidgetsFlutterBinding.ensureInitialized();
+    if (enableCrashLogging) {
+      FirebaseCrashlytics.instance.recordError(object, trace);
+    }
+  });
+}
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,25 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/app.dart';
 import 'package:flutter_template/architecture.dart';
 import 'package:flutter_template/di/environments.dart';
 import 'package:flutter_template/di/injectable.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_template/app.dart';
+import 'package:flutter_template/main_common.dart';
 import 'package:flutter_template/util/env/flavor_config.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await initArchitecture();
-  const values = FlavorValues(
-    baseUrl: 'https://jsonplaceholder.typicode.com/',
-    logNetworkInfo: false,
-    showFullErrorMessages: false,
-  );
-  FlavorConfig(
-    flavor: Flavor.PROD,
-    name: 'PROD',
-    color: Colors.transparent,
-    values: values,
-  );
-  await configureDependencies(Environments.PROD);
-  runApp(MyApp());
+  await wrapMain(() async {
+    await initArchitecture();
+    const values = FlavorValues(
+      baseUrl: 'https://jsonplaceholder.typicode.com/',
+      logNetworkInfo: false,
+      showFullErrorMessages: false,
+    );
+    FlavorConfig(
+      flavor: Flavor.PROD,
+      name: 'PROD',
+      color: Colors.transparent,
+      values: values,
+    );
+    await configureDependencies(Environments.PROD);
+    runApp(MyApp());
+  }, enableCrashLogging: true);
 }
