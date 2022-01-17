9582f103b4277d7dca9dac46fddf54b8b9f73045
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-01-17 11:44:09 +0100

    Updated the inspectors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_template/app.dart';
 import 'package:flutter_template/di/environments.dart';
 import 'package:flutter_template/di/injectable.dart';
@@ -8,10 +7,6 @@ import 'package:flutter_template/util/env/flavor_config.dart';
 import 'package:flutter_template/util/inspector/database_inspector.dart';
 import 'package:flutter_template/util/inspector/local_storage_inspector.dart';
 import 'package:flutter_template/util/inspector/niddler.dart';
-import 'package:get_it/get_it.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:storage_inspector/storage_inspector.dart';
 
 Future<void> main() async {
   await wrapMain(() async {
@@ -31,12 +26,7 @@ Future<void> main() async {
     print('Starting app from main.dart');
     await configureDependencies(Environments.dev);
     await addDatabaseInspector();
-    await initLocalStorageInspector((driver) async {
-      driver.addKeyValueServer(SecureStorageKeyValueServer(GetIt.I.get<FlutterSecureStorage>(), 'Secure Storage', icon: secureStorageIcon));
-      driver.addKeyValueServer(PreferencesKeyValueServer(GetIt.I.get<SharedPreferences>(), 'Shared preferences'));
-      driver.addFileServer(DefaultFileServer((await getApplicationDocumentsDirectory()).path, 'App documents'));
-      driver.addFileServer(DefaultFileServer((await getTemporaryDirectory()).path, 'Cache files'));
-    });
+    await initAllStorageInspectors();
 
     runApp(const MyApp());
   }, enableCrashLogging: false);
--- lib/util/inspector/local_storage_inspector.dart
+++ lib/util/inspector/local_storage_inspector.dart
@@ -1,10 +1,23 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
 import 'package:icapps_architecture/icapps_architecture.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storage_inspector/storage_inspector.dart';
 
-Future<void> initLocalStorageInspector(
+Future<void> initAllStorageInspectors() async {
+  await _initLocalStorageInspector((driver) async {
+    driver.addKeyValueServer(SecureStorageKeyValueServer(GetIt.I.get<FlutterSecureStorage>(), 'Secure Storage', icon: secureStorageIcon));
+    driver.addKeyValueServer(PreferencesKeyValueServer(GetIt.I.get<SharedPreferences>(), 'Shared preferences'));
+    driver.addFileServer(DefaultFileServer((await getApplicationDocumentsDirectory()).path, 'App documents'));
+    driver.addFileServer(DefaultFileServer((await getTemporaryDirectory()).path, 'Cache files'));
+  });
+}
+
+Future<void> _initLocalStorageInspector(
   FutureOr<void> Function(StorageServerDriver) configureServers,
 ) async {
   if (!kDebugMode) return;
@@ -25,4 +38,3 @@ Future<void> initLocalStorageInspector(
 
 const secureStorageIcon =
     r"""<svg height="16" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="#afb1b3" transform="translate(4 2)"><path d="m11.4444444 7c.5522848 0 1 .44771525 1 1v6.8888889c0 .5522847-.4477152 1-1 1h-10.4444444c-.55228475 0-1-.4477153-1-1v-6.8888889c0-.55228475.44771525-1 1-1zm-5.44444441 1.19999999c-1.1045695 0-2 .8954305-2 2.00000001 0 .7402524.40216612 1.3865739.99992751 1.7323937l.0000725 2.0676063h2l.00107749-2.0681882c.59720711-.3459625.9989225-.9919743.9989225-1.7318118 0-1.10456951-.89543051-2.00000001-2-2.00000001z" fill="#afb1b3"/><path d="m2 7v-3c0-2.209139 1.790861-4 4-4s4 1.790861 4 4v3" stroke-width="1.5"/></g></svg>""";
-
