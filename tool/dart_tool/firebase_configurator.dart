f4f4fa3aaa52d545e33a8cb848745d89a3f08f44
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-04-06 20:42:31 +0200

    Remove everything from the main_common.dart

--- tool/dart_tool/firebase_configurator.dart
+++ tool/dart_tool/firebase_configurator.dart
@@ -10,13 +10,42 @@ void main() {
       enableFirebase = result == 'y' || result == 'yes';
     }
   } while (enableFirebase == null);
-  if (enableFirebase){
+  if (enableFirebase) {
     Logger.debug('You can not enable firebase again with this plugin. only remove firebase');
     exit(0);
   }
   _removeLineInFileStartWith('pubspec.yaml', '  firebase_analytics');
   _removeLineInFileStartWith('pubspec.yaml', '  firebase_core');
   _removeLineInFileStartWith('pubspec.yaml', '  firebase_crashlytics');
+  replaceInFile(
+      'lib/main_common.dart',
+      '''
+Future<void> _setupCrashLogging({required bool enabled}) async {
+  if (enabled) {
+    await Firebase.initializeApp();
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(enabled);
+    unawaited(FirebaseCrashlytics.instance.sendUnsentReports());
+  }
+
+  final originalOnError = FlutterError.onError;
+  FlutterError.onError = (errorDetails) async {
+    if (enabled) {
+      await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    }
+    originalOnError?.call(errorDetails);
+  };
+}
+
+''',
+      '\n');
+}
+
+void replaceInFile(String path, String originalString, String newString) {
+  final file = File(path);
+  if (!file.existsSync()) return;
+  final original = file.readAsStringSync();
+  final newContent = original.replaceAll(originalString, newString);
+  file.writeAsStringSync(newContent);
 }
 
 void _removeLineInFileStartWith(String path, String startWithString) {
