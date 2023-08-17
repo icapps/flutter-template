867799403f8f811abeec9ba9ca0edf44f27182bc
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2023-08-17 09:42:38 +0200

    #305: fixed build errors

--- lib/webservice/todo/todo_webservice.g.dart
+++ lib/webservice/todo/todo_webservice.g.dart
@@ -23,7 +23,7 @@ class _TodoWebService implements TodoWebService {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
     final _result =
         await _dio.fetch<List<dynamic>>(_setStreamType<List<Todo>>(Options(
       method: 'GET',
@@ -36,7 +36,11 @@ class _TodoWebService implements TodoWebService {
               queryParameters: queryParameters,
               data: _data,
             )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
     var value = _result.data!
         .map((dynamic i) => Todo.fromJson(i as Map<String, dynamic>))
         .toList();
@@ -54,5 +58,22 @@ class _TodoWebService implements TodoWebService {
       }
     }
     return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
