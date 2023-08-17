504596df8606a56e7c56a3423fe8112a880c855a
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2023-08-17 10:14:56 +0200

    #291: change switch to expression switch

--- lib/util/extension/theme_mode_extension.dart
+++ lib/util/extension/theme_mode_extension.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
 extension ThemeModeExtension on ThemeMode {
-  String get stringValue {
-    switch (this) {
-      case ThemeMode.dark:
-        return 'dark';
-      case ThemeMode.light:
-        return 'light';
-      case ThemeMode.system:
-        return 'system';
-    }
-  }
+  String get stringValue => switch (this) {
+      ThemeMode.dark => 'dark',
+      ThemeMode.light => 'light',
+      ThemeMode.system => 'system',
+    };
 }
