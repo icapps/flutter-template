fc23baf606023cb357f1eac2e248823ccedc850a
Luca Van Zelst <luca.vanzelst@icapps.com>
2023-10-17 10:13:48 +0200

    Updated typo

--- tool/setup/setup.sh
+++ tool/setup/setup.sh
@@ -9,5 +9,5 @@ tool/setup/strip_boilerplate_project.sh || exit -1
 echo "Setup 3/4: Android keystore generator"
 tool/setup/android_keystore_generator.sh || exit -1
 
-echo "Setup 3/4: Removing setup scripts"
+echo "Setup 4/4: Removing setup scripts"
 tool/setup/remove_setup_scripts.sh || exit -1
