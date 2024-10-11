8ad0a2327e90a6a74885b37be0a6d0bcc3d382e9
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2024-10-11 13:44:15 +0200

    #336: used base screen on screens in the app

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_template/di/injectable.dart';
 import 'package:flutter_template/styles/theme_dimens.dart';
 import 'package:flutter_template/util/keys.dart';
 import 'package:flutter_template/viewmodel/login/login_viewmodel.dart';
-import 'package:flutter_template/widget/general/status_bar.dart';
+import 'package:flutter_template/widget/general/simple_screen/base_screen.dart';
 import 'package:flutter_template/widget/general/styled/flutter_template_button.dart';
 import 'package:flutter_template/widget/general/styled/flutter_template_input_field.dart';
 import 'package:flutter_template/widget/general/styled/flutter_template_progress_indicator.dart';
@@ -26,57 +26,46 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return ProviderWidget<LoginViewModel>(
       create: () => getIt()..init(),
-      consumerWithThemeAndLocalization: (context, viewModel, child, theme, localization) => StatusBar.animated(
-        isDarkStyle: theme.isDarkTheme,
-        child: Scaffold(
-          backgroundColor: theme.colorsTheme.background,
-          body: SafeArea(
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(ThemeDimens.padding16),
-              child: Column(
-                children: [
-                  Container(height: ThemeDimens.padding16),
-                  Text(
-                    'Login',
-                    style: theme.coreTextTheme.titleNormal,
-                    textAlign: TextAlign.center,
-                  ),
-                  Container(height: ThemeDimens.padding32),
-                  Text(
-                    'Just fill in some text. There is no validator for the login',
-                    style: theme.coreTextTheme.labelButtonSmall,
-                  ),
-                  Container(height: ThemeDimens.padding32),
-                  FlutterTemplateInputField(
-                    key: Keys.emailInput,
-                    enabled: !viewModel.isLoading,
-                    onChanged: viewModel.onEmailUpdated,
-                    hint: 'Email',
-                  ),
-                  Container(height: ThemeDimens.padding16),
-                  FlutterTemplateInputField(
-                    key: Keys.passwordInput,
-                    enabled: !viewModel.isLoading,
-                    onChanged: viewModel.onPasswordUpdated,
-                    hint: 'Password',
-                  ),
-                  Container(height: ThemeDimens.padding16),
-                  if (viewModel.isLoading) ...[
-                    const FlutterTemplateProgressIndicator.light(),
-                  ] else ...[
-                    FlutterTemplateButton(
-                      key: Keys.loginButton,
-                      isEnabled: viewModel.isLoginEnabled,
-                      text: 'Login',
-                      onClick: viewModel.onLoginClicked,
-                    ),
-                  ],
-                ],
-              ),
-            ),
+      consumerWithThemeAndLocalization: (context, viewModel, child, theme, localization) => BaseScreen(
+        showHeader: false,
+        children: [
+          Container(height: ThemeDimens.padding16),
+          Text(
+            'Login',
+            style: theme.coreTextTheme.titleNormal,
+            textAlign: TextAlign.center,
           ),
-        ),
+          Container(height: ThemeDimens.padding32),
+          Text(
+            'Just fill in some text. There is no validator for the login',
+            style: theme.coreTextTheme.labelButtonSmall,
+          ),
+          Container(height: ThemeDimens.padding32),
+          FlutterTemplateInputField(
+            key: Keys.emailInput,
+            enabled: !viewModel.isLoading,
+            onChanged: viewModel.onEmailUpdated,
+            hint: 'Email',
+          ),
+          Container(height: ThemeDimens.padding16),
+          FlutterTemplateInputField(
+            key: Keys.passwordInput,
+            enabled: !viewModel.isLoading,
+            onChanged: viewModel.onPasswordUpdated,
+            hint: 'Password',
+          ),
+          Container(height: ThemeDimens.padding16),
+          if (viewModel.isLoading) ...[
+            const FlutterTemplateProgressIndicator.light(),
+          ] else ...[
+            FlutterTemplateButton(
+              key: Keys.loginButton,
+              isEnabled: viewModel.isLoginEnabled,
+              text: 'Login',
+              onClick: viewModel.onLoginClicked,
+            ),
+          ],
+        ],
       ),
     );
   }
