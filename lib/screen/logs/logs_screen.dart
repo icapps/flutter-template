7ad2eec43047255b916748be4b3ced81a45ff5bb
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2024-10-15 12:13:30 +0200

    #355: added trigger error

--- lib/screen/logs/logs_screen.dart
+++ lib/screen/logs/logs_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_template/styles/theme_data.dart';
 import 'package:flutter_template/util/extension/date_time_extension.dart';
 import 'package:flutter_template/viewmodel/logs/logs_viewmodel.dart';
 import 'package:flutter_template/widget/general/simple_screen/base_screen.dart';
+import 'package:flutter_template/widget/general/styled/flutter_template_button.dart';
 import 'package:flutter_template/widget/provider/provider_widget.dart';
 import 'package:icapps_architecture/icapps_architecture.dart';
 
@@ -21,19 +22,33 @@ class LogsScreenState extends State<LogsScreen> {
   Widget build(BuildContext context) {
     return ProviderWidget<LogsViewModel>(
       create: () => getIt()..init(),
-      childBuilderWithViewModel: (context, viewModel, theme, localization) => BaseScreen.builder(
+      childBuilderWithViewModel: (context, viewModel, theme, localization) => BaseScreen.child(
         title: 'Logs',
-        itemCount: viewModel.availableDates.length,
-        itemBuilder: (context, index) {
-          final item = viewModel.availableDates[index];
-          return TouchFeedBack(
-            onTapped: () => viewModel.onLogTapped(item),
-            child: Text(
-              item.shortDateWeekdayFormat,
-              style: theme.text.bodyBig,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                itemCount: viewModel.availableDates.length,
+                itemBuilder: (context, index) {
+                  final item = viewModel.availableDates[index];
+                  return TouchFeedBack(
+                    onTapped: () => viewModel.onLogTapped(item),
+                    child: Text(
+                      item.shortDateWeekdayFormat,
+                      style: theme.text.bodyBig,
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
+            FlutterTemplateButton(
+              text: 'Trigger crash',
+              onClick: viewModel.triggerError,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/viewmodel/logs/logs_viewmodel.dart
+++ lib/viewmodel/logs/logs_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_template/navigator/main_navigator.dart';
+import 'package:flutter_template/util/logging/flutter_template_logger.dart';
 import 'package:icapps_architecture/icapps_architecture.dart';
 import 'package:injectable/injectable.dart';
 import 'package:log_to_secure_file/log_to_secure_file.dart';
@@ -22,6 +23,19 @@ class LogsViewModel with ChangeNotifierEx {
     _dates.replaceAll(await _secureLogStorage.availableDates());
     if (disposed) return;
     notifyListeners();
+  }
+
+  void triggerError() {
+    try {
+      throw Exception('This is a test error');
+    } catch (e, trace) {
+      FlutterTemplateLogger.logError(
+        'Error',
+        error: e,
+        stackTrace: trace,
+        sendToFirebase: false,
+      );
+    }
   }
 
   void onLogTapped(DateTime date) => _navigator.goToLogDetailScreen(date: date.toIso8601String());
