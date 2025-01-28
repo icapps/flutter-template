32d4ff58d3867b7b40b25a65c3ac08e56252bf3c
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2025-01-28 10:48:14 +0100

    rename

--- widgetbook/flutter_bootstrap.js
+++ widgetbook/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4008905952"
+    serviceWorkerVersion: "692063274"
   }
 });
