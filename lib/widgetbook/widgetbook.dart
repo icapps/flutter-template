4e97520ec9be098f7a395863d183357f9b92a9d2
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2025-01-28 09:30:06 +0100

    added theme switcher

--- lib/widgetbook/widgetbook.dart
+++ lib/widgetbook/widgetbook.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/architecture.dart';
 import 'package:flutter_template/di/injectable.dart';
+import 'package:flutter_template/styles/theme_data.dart';
 import 'package:flutter_template/util/env/flavor_config.dart';
 import 'package:flutter_template/util/theme/theme_config.dart';
 import 'package:flutter_template/widgetbook/widgetbook.directories.g.dart';
+import 'package:icapps_architecture/icapps_architecture.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
@@ -43,6 +45,23 @@ class WidgetbookApp extends StatelessWidget {
         InspectorAddon(),
         GridAddon(100),
         ZoomAddon(),
+        ThemeAddon(
+          themes: [
+            WidgetbookTheme(
+              name: 'Light',
+              data: FlutterTemplateThemeData.lightTheme,
+            ),
+            WidgetbookTheme(
+              name: 'Dark',
+              data: FlutterTemplateThemeData.darkTheme,
+            ),
+          ],
+          themeBuilder: (context, getTheme, child) {
+            final theme = getTheme(context.isIOSTheme ? TargetPlatform.iOS : TargetPlatform.android);
+            getIt<ThemeConfigUtil>().themeMode = theme.brightness == Brightness.light ? ThemeMode.light : ThemeMode.dark;
+            return child;
+          },
+        ),
       ],
       directories: directories,
     );
